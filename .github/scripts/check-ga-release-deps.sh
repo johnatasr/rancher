898681ab508e5d5163b8520db74b71a07ff3db96
Johnatas <johnatasr@hotmail.com>
2023-09-12 13:14:25 -0300

    more improves
    Signed-off-by: Johnatas <johnatasr@hotmail.com>

--- .github/scripts/check-ga-release-deps.sh
+++ .github/scripts/check-ga-release-deps.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
 set -ue
-
+ 
+release_title=$(echo "${{ github.event.release.name }}" | tr '[:upper:]' '[:lower:]')
 badfiles="false"
 
-for FILE in Dockerfile.dapper go.mod ./package/Dockerfile ./pkg/apis/go.mod ./pkg/settings/setting.go ./scripts/package-env; do
-    if grep -q -E '\-rc[0-9]+' "$FILE"; then
-        badfiles="true"
-        echo "Error: ${FILE} contains rc tags."
+if echo "$release_title" | grep -Eq '^(release v([0-9]{1,2}|100)\.[0-9]{1,100}\.[0-9]{1,100}|v([0-9]{1,2}|100)\.[0-9]{1,100}\.[0-9]{1,100})$'; then
+    for FILE in Dockerfile.dapper go.mod ./package/Dockerfile ./pkg/apis/go.mod ./pkg/settings/setting.go ./scripts/package-env; do
+        if grep -q -E '\-rc[0-9]+' "$FILE"; then
+            badfiles="true"
+            echo "Error: ${FILE} contains rc tags."
+        fi
+
+        if grep -q -Eo 'dev-v[0-9]+\.[0-9]+' "$FILE"; then
+            badfiles="true"
+            echo "Error: ${FILE} contains dev dependencies."
+        fi
+    done
+
+    if [ "${badfiles}" = "true" ]; then
+        echo "Check failed, some files don't match the expected dependencies for a GA release"
+        exit 1
     fi
 
-    if grep -q -Eo 'dev-v[0-9]+\.[0-9]+' "$FILE"; then
-        badfiles="true"
-        echo "Error: ${FILE} contains dev dependencies."
-    fi
-done
-
-if [ "${badfiles}" = "true" ]; then
-  echo "Check failed, some files don't match the expected dependencies for a GA release"
-  exit 1
+    echo "Check completed successfully"
+    return
 fi
 
-echo "Check completed successfully"
+echo "Skipped check"
